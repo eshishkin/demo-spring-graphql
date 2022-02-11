1862cafa16596a199dbc79edeedfe56f0b193b95
Eugene Shishkin <shishkin.john@gmail.com>
2022-02-11 19:34:07 +0400

    Empty service

---
+++ src/test/java/org/eshishkin/edu/demographql/DemoGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package org.eshishkin.edu.demographql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
