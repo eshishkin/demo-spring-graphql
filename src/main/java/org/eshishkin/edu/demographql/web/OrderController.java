d226064e4ccdd890b140b6512649f004d27491f6
Eugene Shishkin <shishkin.john@gmail.com>
2022-03-05 18:50:31 +0400

    Schema mapping for merchants

--- src/main/java/org/eshishkin/edu/demographql/web/OrderController.java
+++ src/main/java/org/eshishkin/edu/demographql/web/OrderController.java
@@ -1,13 +1,16 @@
 package org.eshishkin.edu.demographql.web;
 
+import graphql.schema.DataFetchingEnvironment;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.eshishkin.edu.demographql.model.OrderRequest;
+import org.eshishkin.edu.demographql.persistence.model.MerchantEntity;
 import org.eshishkin.edu.demographql.persistence.model.OrderEntity;
 import org.eshishkin.edu.demographql.service.OrderService;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
@@ -22,6 +25,13 @@ public class OrderController {
     public List<OrderEntity> simple(@Argument Long id) {
         log.info("Query Request [id] - {}", id);
         return orderService.getOrders(id);
+    }
+
+    @SchemaMapping(typeName = "Item", field = "merchant")
+    public MerchantEntity merchant(DataFetchingEnvironment env) {
+        var entity = new MerchantEntity();
+        entity.setName("Default Merchant");
+        return entity;
     }
 
     @MutationMapping("order")
